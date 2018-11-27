cc0a10e5421f4590312bddda31775d7f060212e4
Josh Everett <josh.r.everett@gmail.com>
2018-11-26 23:17:30 -0800

    lol

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const memes = require('./memes');
+const path = require('path');
+
+const PORT = process.env.PORT || 5000;
+const S3_PATH = 'https://s3-us-west-2.amazonaws.com/seemslegit';
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get('/meme', (req, res) => {
+    const meme = req.query.meme;
+    const type = memes[meme];
+    res.redirect(302, `${S3_PATH}/${meme}.${type}`);
+  })
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
